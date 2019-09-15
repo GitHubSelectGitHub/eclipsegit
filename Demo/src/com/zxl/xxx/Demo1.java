ace33ee32f9ed95a5ebed848fcb97f201036e300
yq <select@192.168.2.2>
2019-09-15 18:27:19 +0800

    解决冲突

--- Demo/src/com/zxl/xxx/Demo1.java
+++ Demo/src/com/zxl/xxx/Demo1.java
@@ -3,7 +3,7 @@ package com.zxl.xxx;
 public class Demo1 {
 	public static void main(String[] args) {
 
-		System.out.println("HelloWorld");
+		System.out.println("aaaa+bbbbb");
 		System.out.println("zhangsan");
 		System.out.println("lisi");
 
