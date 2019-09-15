8a24a8b2293ffb6f12839158c60269945e4f1f2c
zxl <52903900+GitHubSelectGitHub@users.noreply.github.com>
2019-09-15 18:55:41 +0800

    Update Demo1.java

--- Demo/src/com/zxl/xxx/Demo1.java
+++ Demo/src/com/zxl/xxx/Demo1.java
@@ -3,7 +3,7 @@ package com.zxl.xxx;
 public class Demo1 {
 	public static void main(String[] args) {
 
-		System.out.println("aaaa+bbbbb");
+		System.out.println("啊啊啊啊啊啊啊啊啊啊啊啊");
 		System.out.println("bbbbb");
 		System.out.println("zhangsan");
 		System.out.println("lisi");
